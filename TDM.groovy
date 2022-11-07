37929e72e26af3cb5140cbdda6cfdbcb107ae2cd
liad kedar <100197454+liadkedar00@users.noreply.github.com>
2022-11-07 15:33:43 +0200

    Update TDM.groovy

--- TDM.groovy
+++ TDM.groovy
@@ -4,27 +4,34 @@ pipeline{
         choice(name:'TDM ENV', choices: ['dev','test','prod'],description:'')
     }
     stages{
-        stage('BitBucket checkout '){
-            steps{
-            }
-        }
+        // stage('BitBucket checkout '){
+        //     steps{
+        //     }
+        // }
         stage('Deploy to instance'){        
             steps{
+                script{
+                }
             }
         }
         stage('Deploy ENV Variables'){
             steps{
+                script{
+                }
                 sh './Deploy_ENV.sh'
-
             }
         }    
         stage('Deploy WSs'){
             steps{
-              sh './Deploy_WSs.sh'
+                script{
+                }
+                sh './Deploy_WSs.sh'
             }
         }
         stage('Deploy LUs'){
             steps{
+                script{
+                }
                 sh './Deploy_LUs.sh'
 
             }
