03b751fe5edc6f3bf7f21bcb121f178375c5ac17
liad kedar <100197454+liadkedar00@users.noreply.github.com>
2022-11-08 12:47:12 +0200

    Update TDM.groovy

--- TDM.groovy
+++ TDM.groovy
@@ -8,9 +8,20 @@ pipeline{
         //     steps{
         //     }
         // }
-        stage('Deploy to instance'){        
+        stage('Copy project code'){        
             steps{
                 script{
+                    sh '''
+                        ssh-keygen -t rsa -f ./.ssh/temp_rsa1 -q -N 'test12'
+                        # set correct rights on temp priv ate keyfile
+                        chmod 600 ./.ssh/temp_rsa1
+                        ssh -F ./cfg/ssh_cfg -i ./cfg/temp_rsa ec2-user@$TARGET_ID "echo 'Sample_Commands';pwd;ls -dal *"
+                        ###
+                        ssh-keygen -t rsa
+                        ssh-copy-id -i .ssh/id_rsa.pub root@10.21.1.11
+                        ssh root@10.21.1.11
+                        ###
+                    '''
                 }
             }
         }
